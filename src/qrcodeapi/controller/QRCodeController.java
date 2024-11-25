3bd3b0de2f61a5cfd892ef89a4054ba1486d6881
kwnna15 <137907037+kwnna15@users.noreply.github.com>
2024-11-25 23:08:16 +0100

    task 3

--- src/qrcodeapi/controller/QRCodeController.java
+++ src/qrcodeapi/controller/QRCodeController.java
@@ -4,14 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import qrcodeapi.service.BufferedImageService;
 
 import java.awt.image.BufferedImage;
+import java.util.Set;
 
 @RestController
 public class QRCodeController {
 
+    private static final Set<String> VALID_FILE_TYPES = Set.of("png", "jpeg", "gif");
     private final BufferedImageService bufferedImageService;
 
     public QRCodeController(BufferedImageService bufferedImageService) {
@@ -24,12 +27,39 @@ public class QRCodeController {
     }
 
     @GetMapping(path = "/api/qrcode")
-    public ResponseEntity<BufferedImage> getImage() {
-        BufferedImage bufferedImage = bufferedImageService.generateWhiteImage(250,250);
+    public ResponseEntity<?> getImage(@RequestParam("size") int size, @RequestParam("type") String type) {
+        //int QRCodeSize = Integer.parseInt(size);
+        if (size < 150 || size > 350) {
+            return ResponseEntity.badRequest()
+                    .body("""
+                            {
+                              "error": "Image size must be between 150 and 350 pixels"
+                            }
+                            """);
+        }
+        if (!VALID_FILE_TYPES.contains(type.toLowerCase())) {
+            return ResponseEntity.badRequest()
+                    .body("""
+                            {
+                              "error": "Only png, jpeg and gif image types are supported"
+                            }
+                            """);
+        }
+
+        BufferedImage bufferedImage = bufferedImageService.generateWhiteImage(size, size);
 
         return ResponseEntity
                 .ok()
-                .contentType(MediaType.IMAGE_PNG)
+                .contentType(getContentType(type))
                 .body(bufferedImage);
+    }
+
+    private static MediaType getContentType(String type) {
+        return switch (type.toLowerCase()) {
+            case "png" -> MediaType.IMAGE_PNG;
+            case "jpeg" -> MediaType.IMAGE_JPEG;
+            case "gif" -> MediaType.IMAGE_GIF;
+            default -> throw new IllegalStateException("Unsupported type=" + type);
+        };
     }
 }
